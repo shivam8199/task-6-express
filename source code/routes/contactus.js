86792e94f160f02e403bebce65154872b2c55181
SHIVAM <angara8199@gmail.com>
2022-10-05 21:52:55 +0530

    first commit

---
+++ source code/routes/contactus.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/contactus', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'contactus.html'));
+});
+
+// /admin/add-product => POST
+router.post('/contactus', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+module.exports = router;
